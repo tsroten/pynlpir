f4fbb1f8e621bffbaed807de452fdf8deaf86d87
Thomas Roten <thomas@roten.us>
2014-03-31 07:18:12 +0800

    Adds unit test for get_key_words().

--- pynlpir/tests/test_nlpir.py
+++ pynlpir/tests/test_nlpir.py
@@ -11,11 +11,11 @@ class TestNLPIR(unittest.TestCase):
     """Unit tests for the pynlpir.nlpir module."""
 
     def test_load_library(self):
-        """Tests that the load_library() method works as expected."""
+        """Tests that the load_library() function works as expected."""
         self.assertTrue(isinstance(nlpir.libNLPIR, ctypes.CDLL))
 
     def test_segment(self):
-        """Tests that the segment() method works as expected."""
+        """Tests that the segment() function works as expected."""
         s = '我们都是美国人。'
         seg_s = nlpir.segment(s, False)
         pos_seg_s = nlpir.segment(s, True)
@@ -24,3 +24,13 @@ class TestNLPIR(unittest.TestCase):
                               ('美国', 'nsf'), ('人', 'n'), ('。', 'wj')]
         self.assertEqual(expected_seg_s, seg_s)
         self.assertEqual(expected_pos_seg_s, pos_seg_s)
+
+    def test_get_key_words(self):
+        """Tests that the get_key_words() function works as expected."""
+        s = '我们都是美国人。'
+        key_words = nlpir.get_key_words(s)
+        weighted_key_words = nlpir.get_key_words(weighted=True)
+        expected_key_words = ['美国']
+        expected_weighted_key_words = [('美国', 0.01)]
+        self.assertEqual(expected_key_words, key_words)
+        self.assertEqual(expected_weighted_key_words, weighted_key_words)
