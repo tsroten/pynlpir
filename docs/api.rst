fb1d9e67357b6720e0840c74b325607c7b69de83
Thomas Roten <thomas@roten.us>
2014-03-31 07:04:29 +0800

    Switches to function-based interface.

--- docs/api.rst
+++ docs/api.rst
@@ -2,11 +2,81 @@ PyNLPIR API
 ===========
 
 .. module:: pynlpir.nlpir
+
 ``pynlpir.nlpir``
----------------
+-----------------
 
 Provides a Python API to NLPIR/ICTCLAS.
 
-.. autoclass:: NLPIR(lib_dir, data_dir, encoding='utf-8', has_traditional=False, pos='ict2')
-    :members:
-    :exclude-members: decode, encode, is_unicode
+.. data:: PACKAGE_DIR
+
+    The absolute path to this package (used by NLPIR to find its ``Data`` directory).
+
+.. data:: LIB_DIR
+
+    The absolute path to this path's lib directory.
+
+.. data:: libNLPIR
+
+    A CDLL instance for the NLPIR API library.
+
+.. function:: load_library(platform, is_64bit, lib_dir=LIB_DIR)
+
+    Loads the NLPIR library appropriate for the user's system.
+
+    :param str platform: The platform identifier for the user's system.
+    :param bool is_64bit: Whether or not the user's system is 64-bit.
+    :param str lib_dir: The directory that contains the library files
+        (defaults to :data:`LIB_DIR`).
+
+.. function:: get_func(name, argtypes=None, restype=None, lib=libNLPIR)
+
+    Retrieves the corresponding NLPIR function.
+
+    :param str name: The name of the NLPIR function to get.
+    :param list argtypes: A list of :mod:`ctypes` data types that correspond
+        to the function's argument types.
+    :param restype: A :mod:`ctypes` data type that corresponds to the
+        function's return type (only needed if the return type isn't
+        :class:`ctypes.c_int`).
+    :param lib: A :class:`ctypes.CDLL` instance for the NLPIR API library where
+        the function will be retrieved from (defaults to :data:`libNLPIR`).
+
+.. function:: init(data_dir=PACKAGE_DIR)
+
+    Initializes the NLPIR API.
+
+    :param str data_dir: The absolute path to the directory that has NLPIR's
+        `Data` directory (defaults to :data:`PACKAGE_DIR`).
+
+.. function:: close
+
+    Exits the NLPIR API and frees allocated memory.
+
+.. function:: segment(s, pos_tagging=True)
+
+    Segment Chinese text *s* using NLPIR.
+
+    The segmented tokens are returned as a list. Each item of the list is a
+    string if *pos_tagging* is `False`, e.g. ``['我们', '是', ...]``. If
+    *pos_tagging* is `True`, then each item is a tuple (``(token, pos)``), e.g.
+    ``[('我们', 'rr'), ('是', 'vshi'), ...]``.
+
+    :param s: The Chinese text to segment. *s* should be Unicode or a UTF-8
+        encoded string.
+    :param bool pos_tagging: Whether or not to include part of speech tagging
+        (defaults to ``True``).
+
+.. function:: get_key_words(s, max_words=50, weighted=False)
+
+    Determines key words in Chinese text *s*.
+
+    The key words are returned in a list. If *weighted* is ``True``,
+    then each list item is a tuple: ``(word, weight)``, where
+    *weight* is a float. If it's *False*, then each list item is a string.
+
+    :param s: The Chinese text analyze. *s* should be Unicode or a UTF-8
+        encoded string.
+    :param int max_words: The maximum number of key words (up to ``50``) to
+        find (defaults to ``50``).
+    :param bool weighted: Whether or not to return the key words' weights.
--- pynlpir/nlpir.py
+++ pynlpir/nlpir.py
@@ -5,233 +5,194 @@ import logging
 import os
 import sys
 
-logger = logging.getLogger('pynlpir')
+logger = logging.getLogger('pynlpir.nlpir')
 
 is_python3 = sys.version_info[0] > 2
+if is_python3:
+    unicode = str
 
-is_linux = sys.platform.startswith('linux')
-is_windows = sys.platform.startswith('win')
-is_64bit = sys.maxsize > 2**32
-logger.debug("Loading NLPIR module on a %s %s system." % ('64-bit' if is_64bit
-             else '32-bit', sys.platform))
-if not is_linux and not is_windows:
-    logger.error("Platform '%s' is not supported by NLPIR." % sys.platform)
-
-#: The absolute path to this package (used by NLPIR to find the ``Data``
+#: The absolute path to this package (used by NLPIR to find its ``Data``
 #: directory).
 PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 #: The absolute path to this path's lib directory.
 LIB_DIR = os.path.join(PACKAGE_DIR, 'lib')
 
-# NLPIR encoding constants:
+#: NLPIR UTF-8 encoding constant.
 UTF8 = 1
 
-# Part of speech mapping constants:
-ICT_2 = 0
-ICT_1 = 1
-PKU_2 = 2
-PKU_1 = 3
-POS = {'ict2': ICT_2, 'ict1': ICT_1, 'pku2': PKU_2, 'pku1': PKU_1}
 
+def load_library(platform, is_64bit, lib_dir=LIB_DIR):
+    """Loads the NLPIR library appropriate for the user's system.
 
-class NLPIR(object):
-    """A Python interface to NLPIR.
-
-    :param str lib_dir: The directory containing the NLPIR library files.
-        Defaults to the library directory that is included with this Python
-        package.
-    :param str data_dir: The directory containing NLPIR's ``Data`` directory.
-        Defaults to this package's root directory.
-    :param str pos: The part of speech map to use. This should be one of:
-        ``'ict1'``, ``'ict2'``, ``'pku1'``, or ``'pku2'``.
+    :param str platform: The platform identifier for the user's system.
+    :param bool is_64bit: Whether or not the user's system is 64-bit.
+    :param str lib_dir: The directory that contains the library files
+        (defaults to :data:`LIB_DIR`).
 
     """
+    logger.info("Loading the NLPIR library file.")
+    logger.debug("Loading library file from '%s'" % lib_dir)
+    if platform.startswith('win') and is_64bit:
+        lib = os.path.join(lib_dir, 'NLPIR64')
+        logger.debug("Using library file for 64-bit Windows.")
+    elif platform.startswith('win'):
+        lib = os.path.join(lib_dir, 'NLPIR32')
+        logger.debug("Using library file for 32-bit Windows.")
+    elif platform.startswith('linux') and is_64bit:
+        lib = os.path.join(lib_dir, 'libNLPIR64.so')
+        logger.debug("Using library file for 64-bit GNU/Linux.")
+    elif platform.startswith('linux'):
+        lib = os.path.join(lib_dir, 'libNLPIR32.so')
+        logger.debug("Using library file for 32-bit GNU/Linux.")
+    else:
+        raise RuntimeError("Platform '%s' is not supported by NLPIR." %
+                           platform)
+    libNLPIR = cdll.LoadLibrary(lib)
+    logger.debug("Library file '%s' loaded." % lib)
+    logger.info("NLPIR library file loaded.")
+    return libNLPIR
 
-    def __init__(self, lib_dir=LIB_DIR, data_dir=PACKAGE_DIR, pos='ict2'):
-        """Loads the NLPIR library; initializes the API; sets the POS map."""
-        self.logger = logging.getLogger('pynlpir.%s' % self.__class__.__name__)
-        self.logger.debug("Initializing a NLPIR object: {'lib_dir': '%s', "
-                          "'data_dir': '%s', 'pos': '%s'}." %
-                          (lib_dir, data_dir, pos))
-        self.pos = pos  # TODO: make this set to default NLPIR POS map.
-        self._load_library(lib_dir)
-        self._init(data_dir)
-        self.set_pos_map(pos)
 
-    def _load_library(self, lib_dir=LIB_DIR):
-        """Loads the NLPIR library appropriate for the user's system.
+is_64bit = sys.maxsize > 2**32
+#: A CDLL instance for the NLPIR API library.
+libNLPIR = load_library(sys.platform, is_64bit)
 
-        :param str lib_dir: The directory that contains the library files.
 
-        """
-        self.logger.info("Loading the NLPIR library file.")
-        self.logger.debug("Loading library file from '%s'" % lib_dir)
-        if is_windows and is_64bit:
-            lib = os.path.join(lib_dir, 'NLPIR64')
-            self.logger.debug("Using library file for 64-bit Windows.")
-        elif is_windows:
-            lib = os.path.join(lib_dir, 'NLPIR32')
-            self.logger.debug("Using library file for 32-bit Windows.")
-        elif is_linux and is_64bit:
-            lib = os.path.join(lib_dir, 'libNLPIR64.so')
-            self.logger.debug("Using library file for 64-bit GNU/Linux.")
-        elif is_linux:
-            lib = os.path.join(lib_dir, 'libNLPIR32.so')
-            self.logger.debug("Using library file for 32-bit GNU/Linux.")
-        else:
-            raise RuntimeError("Platform '%s' is not supported by NLPIR." %
-                               sys.platform)
-        self.libNLPIR = cdll.LoadLibrary(lib)
-        self.logger.debug("Library file '%s' loaded." % lib)
-        self.logger.info("NLPIR library file loaded.")
+def get_func(name, argtypes=None, restype=None, lib=libNLPIR):
+    """Retrieves the corresponding NLPIR function.
 
-    def _init(self, data_dir=PACKAGE_DIR):
-        """Initializes the NLPIR API."""
-        self.logger.info("Initializing the NLPIR API.")
-        self.logger.debug("Initializing the NLPIR API: {'data_dir': '%s'}"
-                          % data_dir)
-        init = self.get_func('NLPIR_Init')
-        if not init(data_dir, UTF8):
-            raise RuntimeError("NLPIR function 'NLPIR_Init' failed. "
-                               "Check NLPIR error log for more information.")
-        else:
-            self.logger.info("NLPIR API initialized.")
-
-    def _exit(self):
-        """Exits the NLPIR API and frees allocated memory."""
-        self.logger.debug("Exiting the NLPIR API.")
-        _exit = self.get_func('NLPIR_Exit', restype=c_bool)
-        if not _exit():
-            self.logger.warning("NLPIR function 'NLPIR_Exit' failed.")
-        else:
-            self.logger.debug("NLPIR API exited.")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        """Exits the NLPIR API when a context manager is used."""
-        self.close()
-
-    def close(self):
-        """Exits the NLPIR API."""
-        self._exit()
-        del self.libNLPIR._handle
-        del self.libNLPIR
-
-    def _is_unicode(self, s):
-        """Checks if *s* is unicode (str in Python 3)."""
-        if is_python3:
-            return isinstance(s, str)
-        return isinstance(s, unicode)
-
-    def _decode(self, s):
-        """Decodes *s* using UTF-8."""
-        return s if self._is_unicode(s) else s.decode('utf-8')
-
-    def _encode(self, s):
-        """Encodes *s* using UTF-8."""
-        return s.encode('utf-8') if self._is_unicode(s) else s
-
-    def get_func(self, name, argtypes=None, restype=None):
-        """Retrieves the corresponding NLPIR function.
-
-        *name* is a string containing the name of the NLPIR function to get.
-        *argtypes* is a list of :mod:`ctypes` data types that correspond to the
-        function's argument types.
-        *restype* is a :mod:`ctypes` data type that corresponds to the
+    :param str name: The name of the NLPIR function to get.
+    :param list argtypes: A list of :mod:`ctypes` data types that correspond
+        to the function's argument types.
+    :param restype: A :mod:`ctypes` data type that corresponds to the
         function's return type (only needed if the return type isn't
         :class:`ctypes.c_int`).
+    :param lib: A :class:`ctypes.CDLL` instance for the NLPIR API library where
+        the function will be retrieved from (defaults to :data:`libNLPIR`).
 
-        """
-        self.logger.debug("Getting NLPIR API function: {'name': '%s', "
-                          "'argtypes': '%s', 'restype': '%s'}." %
-                          (name, argtypes, restype))
-        func = getattr(self.libNLPIR, name)
-        if argtypes is not None:
-            func.argtypes = argtypes
-        if restype is not None:
-            func.restype = restype
-        self.logger.debug("NLPIR API function '%s' retrieved." % name)
-        return func
+    """
+    logger.debug("Getting NLPIR API function: {'name': '%s', 'argtypes': '%s',"
+                 " 'restype': '%s'}." % (name, argtypes, restype))
+    func = getattr(lib, name)
+    if argtypes is not None:
+        func.argtypes = argtypes
+    if restype is not None:
+        func.restype = restype
+    logger.debug("NLPIR API function '%s' retrieved." % name)
+    return func
 
-    def set_pos_map(self, pos):
-        """Changes the current part of speech map.
 
-        :param str pos_map: Which part of speech map to use. Must be one of:
-            ``'ict1'``, ``'ict2'``, ``'pku1'``, or ``'pku2'``.
+def init(data_dir=PACKAGE_DIR):
+    """Initializes the NLPIR API.
 
-        """
-        self.logger.info("Setting POS map to '%s'." % pos)
-        pos_map = pos.lower().replace('_', '').replace('-', '')
-        try:
-            pos_cons = POS[pos_map]
-        except KeyError:
-            raise ValueError("POS map '%s' not supported by NLPIR." % pos)
-        _set_pos_map = self.get_func('NLPIR_SetPOSmap', argtypes=[c_int])
-        if not _set_pos_map(pos_cons):
-            self.logger.error("Unable to set POS map to '%s'." % pos)
-        else:
-            self.logger.info("POS map set to '%s'." % pos)
-            self.pos = pos
+    :param str data_dir: The absolute path to the directory that has NLPIR's
+        `Data` directory (defaults to :data:`PACKAGE_DIR`).
 
-    def segment(self, s, pos_tagging=True):
-        """Segment Chinese text *s* using NLPIR.
+    """
+    logger.info("Initializing the NLPIR API.")
+    logger.debug("Initializing the NLPIR API: {'data_dir': '%s'}" % data_dir)
+    init = get_func(libNLPIR, 'NLPIR_Init')
+    if not init(data_dir, UTF8):
+        raise RuntimeError("NLPIR function 'NLPIR_Init' failed.")
+    else:
+        logger.info("NLPIR API initialized.")
 
-        The segmented tokens are returned in a list. If *pos_tagging* is
-        ``True``, then each token is returned as a tuple: ``(token, pos)``.
-        If it's ``False``, then each token is returned as a string.
 
-        """
-        s = self._decode(s)
-        self.logger.debug("Segmenting text with%s POS tagging: %s." %
-                          ('' if pos_tagging else 'out', s))
-        _process_paragraph = self.get_func('NLPIR_ParagraphProcess',
-                                           [c_char_p, c_int], c_char_p)
-        result = _process_paragraph(self._encode(s), pos_tagging)
-        result = result.decode('utf-8')
-        self.logger.debug("Finished segmenting text: %s." % result)
-        self.logger.debug("Formatting segmented text.")
-        tokens = result.strip().split(' ')
-        if pos_tagging:
-            tokens = [tuple(t.split('/')) for t in tokens]
-        self.logger.debug("Formatted segmented text: %s." % tokens)
-        return tokens
+def close():
+    """Exits the NLPIR API and frees allocated memory."""
+    logger.debug("Exiting the NLPIR API.")
+    exit = get_func('NLPIR_Exit', restype=c_bool)
+    if not exit():
+        logger.warning("NLPIR function 'NLPIR_Exit' failed.")
+    else:
+        logger.debug("NLPIR API exited.")
 
-    def get_key_words(self, s, max_words=50, weighted=False):
-        """Determines key words in Chinese text *s*.
 
-        The key words are returned in a list. If *weighted* is ``True``,
-        then each token is returned as a tuple: ``(token, weight)``, where
-        *weight* is a float. If it's *False*, then each token is returned as a
-        string.
+def _decode(s):
+    """Decodes *s* using UTF-8."""
+    return s if isinstance(s, unicode) else s.decode('utf-8')
 
-        """
-        def to_float(s):
-            try:
-                n = float(s)
-                return n
-            except ValueError:
-                return False
 
-        s = self._decode(s)
-        self.logger.debug("Searching for up to %s%s key words in: %s." %
-                          (max_words, ' weighted' if weighted else '', s))
-        _get_key_words = self.get_func('NLPIR_GetKeyWords',
-                                       [c_char_p, c_int, c_bool], c_char_p)
-        result = _get_key_words(self._encode(s), max_words, weighted)
-        result = result.decode('utf-8')
-        self.logger.debug("Finished key word search: %s." % result)
-        self.logger.debug("Formatting key word search results.")
-        fresult = result.strip('#').split('#')
-        if weighted:
-            weights, words = [], []
-            for w in fresult:
-                word, pos, weight = w.split('/')
-                weight = to_float(weight)
-                weights.append(weight or 0.0)
-                words.append(word)
-            fresult = zip(words, weights)
-        self.logger.debug("Key words formatted: %s." % fresult)
-        return fresult
+def _encode(s):
+    """Encodes *s* using UTF-8."""
+    return s.encode('utf-8') if isinstance(s, unicode) else s
+
+
+def _to_float(s):
+    """Converts *s* to a float if possible; if not, returns `False`."""
+    try:
+        f = float(s)
+        return f
+    except ValueError:
+        return False
+
+
+def segment(s, pos_tagging=True):
+    """Segment Chinese text *s* using NLPIR.
+
+    The segmented tokens are returned as a list. Each item of the list is a
+    string if *pos_tagging* is `False`, e.g. ``['我们', '是', ...]``. If
+    *pos_tagging* is `True`, then each item is a tuple (``(token, pos)``), e.g.
+    ``[('我们', 'rr'), ('是', 'vshi'), ...]``.
+
+    :param s: The Chinese text to segment. *s* should be Unicode or a UTF-8
+        encoded string.
+    :param bool pos_tagging: Whether or not to include part of speech tagging
+        (defaults to ``True``).
+
+    """
+    s = _decode(s)
+    logger.debug("Segmenting text with%s POS tagging: %s." %
+                 ('' if pos_tagging else 'out', s))
+    _process_paragraph = get_func('NLPIR_ParagraphProcess', [c_char_p, c_int],
+                                  c_char_p)
+    result = _process_paragraph(_encode(s), pos_tagging)
+    result = result.decode('utf-8')
+    logger.debug("Finished segmenting text: %s." % result)
+    logger.debug("Formatting segmented text.")
+    tokens = result.strip().split(' ')
+    if pos_tagging:
+        tokens = [tuple(t.split('/')) for t in tokens]
+    logger.debug("Formatted segmented text: %s." % tokens)
+    return tokens
+
+
+def get_key_words(s, max_words=50, weighted=False):
+    """Determines key words in Chinese text *s*.
+
+    The key words are returned in a list. If *weighted* is ``True``,
+    then each list item is a tuple: ``(word, weight)``, where
+    *weight* is a float. If it's *False*, then each list item is a string.
+
+    :param s: The Chinese text analyze. *s* should be Unicode or a UTF-8
+        encoded string.
+    :param int max_words: The maximum number of key words (up to ``50``) to
+        find (defaults to ``50``).
+    :param bool weighted: Whether or not to return the key words' weights.
+
+    """
+    s = _decode(s)
+    logger.debug("Searching for up to %s%s key words in: %s." %
+                 (max_words, ' weighted' if weighted else '', s))
+    _get_key_words = get_func('NLPIR_GetKeyWords', [c_char_p, c_int, c_bool],
+                              c_char_p)
+    result = _get_key_words(_encode(s), max_words, weighted)
+    result = result.decode('utf-8')
+    logger.debug("Finished key word search: %s." % result)
+    logger.debug("Formatting key word search results.")
+    fresult = result.strip('#').split('#')
+    if weighted:
+        weights, words = [], []
+        for w in fresult:
+            word, pos, weight = w.split('/')
+            weight = _to_float(weight)
+            weights.append(weight or 0.0)
+            words.append(word)
+        fresult = zip(words, weights)
+    logger.debug("Key words formatted: %s." % fresult)
+    return fresult
+
+
+# Initialize the NLPIR API.
+init()
