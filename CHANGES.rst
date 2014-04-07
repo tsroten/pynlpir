ff42cf67b5faf4cca15d4cb8bf0393732b5b7796
Thomas Roten <thomas@roten.us>
2014-04-07 22:24:54 +0800

    Bumps version to v0.1.1.

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,3 +5,8 @@ v0.1 (2014-04-07)
 -----------------
 
 * Initial release.
+
+v0.1.1 (2014-04-07)
+-------------------
+
+* Fixes installation problem with package data.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name='PyNLPIR',
-    version='0.1.1dev',
+    version='0.1.1',
     author='Thomas Roten',
     author_email='thomas@roten.us',
     url='https://github.com/tsroten/pynlpir',
