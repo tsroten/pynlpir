563d04ba6075f1d6c8d2296cb6a355e442e0b0e9
Thomas Roten <thomas@roten.us>
2014-12-18 08:08:50 -0500

    Adds notes about Python 3.4 support.

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Education',
         'Topic :: Text Processing :: Linguistic',
         'Topic :: Software Development :: Libraries :: Python Modules',
