059222d415c9b4a829386e7bdf6a03195269331e
Thomas Roten <thomas@roten.us>
2015-07-12 06:53:00 -0700

    Adds NWI functions.

--- pynlpir/tests/test_nlpir.py
+++ pynlpir/tests/test_nlpir.py
@@ -2,9 +2,12 @@
 """Unit tests for pynlpir.nlpir."""
 from __future__ import unicode_literals
 import ctypes
+import os
 import unittest
 
-from pynlpir import nlpir
+import pynlpir
+nlpir = pynlpir.nlpir
+pynlpir.open()
 
 
 class TestNLPIR(unittest.TestCase):
@@ -13,3 +16,13 @@ class TestNLPIR(unittest.TestCase):
     def test_load_library(self):
         """Tests that the load_library() function works as expected."""
         self.assertTrue(isinstance(nlpir.libNLPIR, ctypes.CDLL))
+
+    def test_nwi(self):
+        """Tests that the basic new word identification functions work."""
+        data_dir = os.path.join(os.path.dirname(__file__), 'data')
+        test_file = os.path.join(data_dir, 'nwi-test.txt')
+        self.assertTrue(nlpir.NWI_Start())
+        self.assertTrue(nlpir.NWI_AddFile(test_file))
+        self.assertTrue(nlpir.NWI_Complete())
+        self.assertEqual('撒门#西生#撒督#',
+                         nlpir.NWI_GetResult(False).decode('utf-8'))
